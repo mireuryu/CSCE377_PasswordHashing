5c1738dfa135a0edf7d3343e23cbbf8ebc7e7047
mireuryu <126295813+mireuryu@users.noreply.github.com>
2025-10-26 19:52:32 -0500

    Update data.py
    fetch/push errors, commented out the code

--- data/data.py
+++ data/data.py
@@ -1,8 +1,8 @@
-from huggingface_hub import login
-login(token="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")
+#from huggingface_hub import login
+#login(token="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")
 
-from datasets import load_dataset
+#from datasets import load_dataset
 
-dataset = load_dataset("InfinitodeLTD/PWLDS")
+#dataset = load_dataset("InfinitodeLTD/PWLDS")
 
-print(dataset)
+#print(dataset)
