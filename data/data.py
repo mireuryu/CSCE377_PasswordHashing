531df2be5ff01cfb5d81d79a90c96badebcd51d1
mireuryu <126295813+mireuryu@users.noreply.github.com>
2025-11-30 14:47:11 -0600

    Delete data/data.py

--- data/data.py
+++
@@ -1,8 +0,0 @@
-#from huggingface_hub import login
-#login(token="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")
-
-#from datasets import load_dataset
-
-#dataset = load_dataset("InfinitodeLTD/PWLDS")
-
-#print(dataset)
